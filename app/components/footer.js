928004373c2dbd145dfbfce95fd3c9b765bee59f
Jhemming93 <97263089+Jhemming93@users.noreply.github.com>
2024-04-30 07:10:09 -0600

    Update footer.js

--- app/components/footer.js
+++ app/components/footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
       <div className="flex sm:container mx-auto" style={{ maxWidth: 1024 }}>
         <div className="flex p-2">
           <p className="pl-1 self-center text-lrg font-bold" id="disclaimer">
-            *This tool is for estimate purposes ionly and doesn't offer a
+            *This tool is for estimate purposes only and does not offer a
             guarentee of price or offer any trueness on any resemblences to any
             company that this tool may resemble.{" "}
           </p>
